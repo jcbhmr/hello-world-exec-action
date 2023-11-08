d87cc48ac6f6680510430d0275814ad575e881bd
Jacob Hummer <jcbhmr@outlook.com>
2023-11-08 15:00:56 -0600

    Update _main.mjs

--- _main.mjs
+++ _main.mjs
@@ -1,35 +1,28 @@
 import { dirname, join } from "node:path";
-import { createWriteStream } from "node:fs";
-import { pipeline } from "node:stream/promises";
 import { spawn } from "node:child_process";
 import { once } from "node:events";
 import { mkdir } from "node:fs/promises";
 const name = "main";
 const tag = "v1.0.0";
-const localName = process.platform === "windows" ? `${name}.exe` : name;
+const binName = process.platform === "windows" ? `${name}.exe` : name;
 let file;
 if (dirname(process.argv[1]).startsWith(process.cwd())) {
-  file = join(dirname(process.argv[1]), "target", "debug", localName);
+  file = join(dirname(process.argv[1]), "target", "debug", binName);
 } else {
   const target = {
     "win32,x64": "x86_64-pc-windows-msvc",
     "darwin,x64": "x86_64-apple-darwin",
     "linux,x64": "x86_64-unknown-linux-gnu",
   }[[process.platform, process.arch].toString()];
-  const remoteName =
-    process.platform === "windows"
-      ? `${name}-${target}.exe`
-      : `${name}-${target}`;
-  file = join(dirname(process.argv[1]), "target", "release", localName);
+  const url = `https://github.com/${process.env.GITHUB_ACTION_REPOSITORY}/releases/download/${tag}/${target}.zip`;
+  file = join(dirname(process.argv[1]), "target", "release", binName);
   await mkdir(dirname(file), { recursive: true });
-  const response = await fetch(
-    `https://github.com/${process.env.GITHUB_ACTION_REPOSITORY}/releases/download/${tag}/${remoteName}`
+  const subprocess1 = spawn(
+    `curl -fsSL "$url" | unzip -d "$dir"`,
+    { shell: "bash", env: { ...process.env, url, bin: dirname(file) } },
   );
-  await pipeline(response.body, createWriteStream(file));
+  await once(subprocess1, "exit");
 }
-const subprocess = spawn(file, {
-  stdio: "inherit",
-  env: { ...process.env, file },
-});
-await once(subprocess, "spawn");
-subprocess.on("exit", (x) => process.exit(x));
+const subprocess2 = spawn(file, { stdio: "inherit" });
+await once(subprocess2, "spawn");
+subprocess2.on("exit", (x) => process.exit(x));
