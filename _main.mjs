9306a330618bd1242349ee4749209e34fbaeb85a
Jacob Hummer <jcbhmr@outlook.com>
2023-11-08 21:33:50 -0600

    save

--- _main.mjs
+++ _main.mjs
@@ -1,7 +1,9 @@
 import { dirname, join } from "node:path";
 import { spawn } from "node:child_process";
 import { once } from "node:events";
-import { existsSync } from "node:fs";
+import { createReadStream, createWriteStream, existsSync } from "node:fs";
+import { copyFile } from "node:fs/promises";
+import { createGunzip } from "node:zlib";
 const name = "main";
 const target = {
   "win32,x64": "x86_64-pc-windows-msvc",
@@ -9,9 +11,21 @@ const target = {
   "linux,x64": "x86_64-unknown-linux-gnu",
 }[[process.platform, process.arch].toString()];
 const exe = process.platform === "win32" ? ".exe" : "";
-const file = [
-  join(dirname(process.argv[1]), "target", target, "release", name + exe),
-  join(dirname(process.argv[1]), "target", "debug", name + exe),
-].find((f) => existsSync(f));
+const __dirname = dirname(process.argv[1]);
+let file;
+if (dirname(process.argv[1]).startsWith(process.cwd())) {
+  file = join(dirname(process.argv[1]), "target", "debug", name + exe);
+} else {
+  file = join(
+    dirname(process.argv[1]),
+    "target",
+    target,
+    "release",
+    name + exe
+  );
+  const gz = join(dirname(file), name + exe + ".gz");
+  await copyFile(file, gz);
+  await pipeline(createReadStream(gz), createGunzip(), createWriteStream(file));
+}
 const subprocess = spawn(file, { stdio: "inherit" });
 process.exitCode = (await once(subprocess, "exit"))[0];
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use actions_core as core;
+use chrono::prelude::*;
 fn main() {
-    println!("Hello, world!");
+    println!("Hello {}!", core::get_input("name"));
+    core::set_output("time", &Local::now().format("%H:%M:%S").to_string());
 }
