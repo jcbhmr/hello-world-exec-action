9a9c2991bc61ec2e4101b5c666bfaa301f0a4e57
Jacob Hummer <jcbhmr@outlook.com>
2023-11-08 18:05:20 -0600

    fix some things

--- _main.mjs
+++ _main.mjs
@@ -4,23 +4,23 @@ import { once } from "node:events";
 import { mkdir } from "node:fs/promises";
 import assert from "node:assert/strict";
 const name = "main";
-const release = "v1.0.0";
-const binName = process.platform === "windows" ? `${name}.exe` : name;
+const release = "v1.0.1";
+const exe = process.platform === "windows" ? ".exe" : "";
 let file;
 if (dirname(process.argv[1]).startsWith(process.cwd())) {
-  file = join(dirname(process.argv[1]), "target", "debug", binName);
+  file = join(dirname(process.argv[1]), "target", "debug", name + exe);
 } else {
-  file = join(dirname(process.argv[1]), "target", "release", binName);
   const target = {
     "win32,x64": "x86_64-pc-windows-msvc",
     "darwin,x64": "x86_64-apple-darwin",
     "linux,x64": "x86_64-unknown-linux-gnu",
   }[[process.platform, process.arch].toString()];
+  file = join(dirname(process.argv[1]), "target", "release", name + exe);
+  await mkdir(dirname(file), { recursive: true });
   const response = await fetch(
-    `https://github.com/${process.env.GITHUB_ACTION_REPOSITORY}/releases/download/${release}/${target}.zip`
+    `https://github.com/${process.env.GITHUB_ACTION_REPOSITORY}/releases/download/${release}/${name}-${target}${exe}`
   );
   assert(response.ok, `${response.status} ${response.url}`);
-  await mkdir(dirname(file), { recursive: true });
   await pipeline(response.body, createWriteStream(file));
   await chmod(file, 0o755);
 }
